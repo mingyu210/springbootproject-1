9dfa03804290137fb57475b389700591ae7941c3
mingyu210 <baemingyu5@gamil.com>
2025-02-09 23:09:09 +0900

    컨트롤러 생성

---
+++ src/main/java/com/example/springproject/controller/FirstController.java
@@ -0,0 +1,10 @@
+package com.example.springproject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class FirstController {
+    public String niceToMeetYou(){
+        return "";
+    }
+}
